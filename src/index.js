66f5ff5acb4f1e7d3bc1d7fc034d4923f235a8dc
mariilo97 <lopezmariana997@gmail.com>
2023-02-07 13:07:11 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import validator from './validator.js';
 
-console.log(validator);
+const boton = document.getElementById("miBoton");
+boton.addEventListener('click', validate);
+
+function validate() {
+  const cardnumber = document.getElementById("cardnumber").value; // capturando valor
+  if (cardnumber === "") { // no tiene valor
+    //alert("Captura el numero de tu tarjeta")
+    document.getElementById("mensaje").textContent = "Captura el Numero de Tarjeta, por favor"
+  }
+  else {
+    valid(cardnumber); // envias el valor a la fn
+    // alert("Validando numero...")
+    document.getElementById("box-valid").style.display = "none" // ocultas la primer caja
+    //mostrar box-result
+    document.getElementById("box-result").style.display = "block"// muestras la segunda caja 
+  }
+}
+function valid() {
+  const result = validator.isValid(document.getElementById("cardnumber").value);
+  const valid = validator.maskify(document.getElementById("cardnumber").value);
+  document.getElementById("cardnumber").value = "";
+  document.getElementById("result-michi").textContent = valid
+
+  if (result === true) {
+    //alert("Tu tarjeta es valida, realiza tu compra ahora")
+    document.getElementById("mensajeUno").textContent = "Tu tarjeta es Valida, Puedes Realizar tu Compra Ahora"
+  }
+  else {
+    //alert("Tu tarjeta es invalida, por favor vuelve a intentarlo")
+    document.getElementById("mensajeUno").textContent = "Tu Tarjeta es Invalida, Por Favor Vuelve a Intentarlo"
+  }
+  // document.getElementById("cardnumber").value = valid; 
+}
+
+//console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,56 @@
 const validator = {
-  // ...
+  isValid: function (tarjeta) {
+    //divide el conjunto de numeros y reverso de los numeros 
+    const division = tarjeta.split("").reverse();
+    const arrayImpares = [];
+    const arrayPares = [];
+    // identificar pares e impares y multiplicarlos
+    for (let i = 0; i < division.length; i++) {
+      if (i % 2 === 1) {
+        if ((division[i] * 2) > 9) {
+          arrayPares.push((division[i] * 2) - 9);
+
+        } else {
+          arrayPares.push(division[i] * 2);
+        }
+
+      }
+      if (i % 2 === 0) {
+        arrayImpares.push(Number(division[i]));
+      }
+    }
+
+    //sumar los array para tener un resultado unico 
+    let nuevoArray = []; {
+      nuevoArray = arrayImpares.concat(arrayPares);
+    }
+
+    //sumar el resultado del arrayNuevo
+    let suma = 0;
+    for (let i = 0; i < nuevoArray.length; i++) {
+      suma += nuevoArray[i];
+    }
+    const resultado = suma % 10 === 0;
+    if (resultado === true) {
+      return true
+    }
+    else {
+      return false
+    }
+  },
+  //maskify- funcion que permite enmascarar los numeros con el simbolo #
+  maskify: (msk) => {
+    const fourSaved = msk.slice(-4);
+    //console.log(fourSaved);
+    let michi = "";
+    for (let i = 0; i < msk.length; i++) {
+      michi = michi + "#";
+    }
+    const forMichi = michi.slice(0, michi.length - 4);
+    const result = forMichi + fourSaved;
+    return result;
+  }
 };
+
 
 export default validator;
